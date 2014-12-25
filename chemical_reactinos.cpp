e88ba13df5e3656c99ae8a850c779dd456d54670
Meiyim <362169764@qq.com>
2014-12-25 17:12:32 +0800

    Create chemical_reactinos.cpp

---
+++ chemical_reactinos.cpp
@@ -0,0 +1,9 @@
+# include"CommonParameters.h"
+#include"ControVolumns.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+string  chemicalReaction( vector< vector<handle> >& CVs, double timeStep) {
+	return"Chemical Reaction Complete;";
+}
