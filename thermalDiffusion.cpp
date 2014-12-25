67b593a2e89656a199bb21f2f7a923c28578d373
Meiyim <362169764@qq.com>
2014-12-25 17:25:27 +0800

    Create thermalDiffusion.cpp

---
+++ thermalDiffusion.cpp
@@ -0,0 +1,8 @@
+# include"CommonParameters.h"
+#include"ControVolumns.h"
+#include <string>
+#include <vector>
+using namespace std;
+string thermalDiffusion( vector< vector<handle> >& CVs, double timeStep) {
+	return "Thermal Diffusion complete.";
+}
